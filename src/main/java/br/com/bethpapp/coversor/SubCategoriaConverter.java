dbd10a8b34a8b0d16b5f48bd9fef1b27a16c8ffd
messias fernandes <messiasfernandes@gmail.com>
2023-05-04 21:40:12 -0300

    criação coversor dto subcategoria

---
+++ src/main/java/br/com/bethpapp/coversor/SubCategoriaConverter.java
@@ -0,0 +1,27 @@
+package br.com.bethpapp.coversor;
+
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import br.com.bethpapp.dominio.entidade.SubCategoria;
+import br.com.bethpapp.modelo.dto.SubCategoriaDTO;
+
+@Component
+public class SubCategoriaConverter {
+	@Autowired
+	private ModelMapper modelMapper;
+
+	public SubCategoriaDTO toDto(SubCategoria objeto) {
+
+		return modelMapper.map(objeto, SubCategoriaDTO.class);
+	}
+	
+	public List<SubCategoriaDTO> toCollectionDto(List<SubCategoria> subcategorias) {
+		return subcategorias.stream().map(this::toDto).collect(Collectors.toList());
+	}
+}
