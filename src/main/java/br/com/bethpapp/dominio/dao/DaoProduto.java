886d943db1ece9e5fe99c0db89bfe6a2739f1e17
messias <messiasfernandes@gmail.com>
2023-05-14 23:21:00 -0300

    criação contas a pagar e contas pagar detalhes

--- src/main/java/br/com/bethpapp/dominio/dao/DaoProduto.java
+++ src/main/java/br/com/bethpapp/dominio/dao/DaoProduto.java
@@ -3,8 +3,10 @@ import br.com.bethpapp.dominio.entidade.Produto;
 import br.com.bethpapp.query.ProdutoQuery;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 @Repository
 public interface DaoProduto extends JpaRepository<Produto, java.lang.Long>, ProdutoQuery {
-
+	@Query("SELECT Count(*)from Produto p where p.codigofabricante =:pfabricante")
+	Long isCodigoFabCadastrado(String pfabricante);
 }
