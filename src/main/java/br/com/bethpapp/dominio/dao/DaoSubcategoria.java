2594d01185125f9caef502fd4d832fd294e5cf8c
messiasfernandes <messiasfernandes@gmail.com>
2023-06-26 11:50:42 -0300

    teste

--- src/main/java/br/com/bethpapp/dominio/dao/DaoSubcategoria.java
+++ src/main/java/br/com/bethpapp/dominio/dao/DaoSubcategoria.java
@@ -1,10 +1,11 @@
 package br.com.bethpapp.dominio.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import br.com.bethpapp.dominio.entidade.SubCategoria;
 @Repository
-public interface DaoSubcategoria  extends JpaRepository<SubCategoria, Long>{
+public interface DaoSubcategoria  extends JpaRepository<SubCategoria, Long>, JpaSpecificationExecutor<SubCategoria>{
 
 }
