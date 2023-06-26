549a91da89c5c23bdd610c4be466082b55686d0e
messiasfernandes <messiasfernandes@gmail.com>
2023-06-26 09:06:18 -0300

    mudanças feitas na estruturas

--- src/main/java/br/com/bethpapp/coversor/SubCategoriaConverter.java
+++ src/main/java/br/com/bethpapp/coversor/SubCategoriaConverter.java
@@ -6,9 +6,12 @@ import java.util.stream.Collectors;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Component;
 
+import br.com.bethpapp.dominio.entidade.Produto;
 import br.com.bethpapp.dominio.entidade.SubCategoria;
+import br.com.bethpapp.modelo.dto.ProdutoDTO;
 import br.com.bethpapp.modelo.dto.SubCategoriaDTO;
 
 @Component
@@ -23,5 +26,9 @@ public class SubCategoriaConverter {
 	
 	public List<SubCategoriaDTO> toCollectionDto(List<SubCategoria> subcategorias) {
 		return subcategorias.stream().map(this::toDto).collect(Collectors.toList());
+	}
+	public Page<SubCategoriaDTO> topage(Page<SubCategoria> objetos) {
+
+		return objetos.map(obj -> toDto(obj));
 	}
 }
