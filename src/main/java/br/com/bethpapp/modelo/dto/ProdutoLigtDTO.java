da5741f70c58ef8e3c29653e985b59a33c7c697d
messias fernandes <messiasfernandes@gmail.com>
2023-07-07 07:00:23 -0300

    adiconando tributo subtotal entidade componente

--- src/main/java/br/com/bethpapp/modelo/dto/ProdutoLigtDTO.java
+++ src/main/java/br/com/bethpapp/modelo/dto/ProdutoLigtDTO.java
@@ -1,9 +1,12 @@
 package br.com.bethpapp.modelo.dto;
 
+import java.math.BigDecimal;
+
 import lombok.Data;
 
 @Data
 public class ProdutoLigtDTO {
 	private String nomeproduto;
 	private EstoqueDTO estoque;
+	private BigDecimal precovenda;
 }
